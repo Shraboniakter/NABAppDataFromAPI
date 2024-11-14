80033f65592ba5dba7f8abfa0dcb4b92576a51b5
shrabon <shrabonbhuiyan708@gmail.com>
2024-11-15 00:58:24 +0600

    API Integration

---
+++ android/app/src/main/kotlin/com/example/nba_app_data_rom_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nba_app_data_rom_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:nba_app_data_rom_api/Home_Page.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
